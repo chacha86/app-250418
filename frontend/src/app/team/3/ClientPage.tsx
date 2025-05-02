83457512034a60150c106b73dc2e6514d2cdf107
chacha86 <taejincha0619@gmail.com>
2025-05-02 09:31:14 +0900

    work

--- frontend/src/app/team/3/ClientPage.tsx
+++ frontend/src/app/team/3/ClientPage.tsx
@@ -7,7 +7,7 @@ export default function ClinetPage() {
     <>
       <div>회원 로그인 페이지</div>
       <LoginForm2
-        url="https://api.zzirit.shop/api/auth/logout"
+        url="https://api.zzirit.shop/api/auth/basic/login"
         nameKey="username"
         passKey="password"
       />
