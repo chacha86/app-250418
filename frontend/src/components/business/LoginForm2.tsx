55856475e526c68c49864bd37075a2b58a52b632
chacha86 <taejincha0619@gmail.com>
2025-05-02 11:20:53 +0900

    work

--- frontend/src/components/business/LoginForm2.tsx
+++ frontend/src/components/business/LoginForm2.tsx
@@ -54,6 +54,7 @@ export default function LoginForm({
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
       body: JSON.stringify({
         [nameKey]: username,
         [passKey]: password,
