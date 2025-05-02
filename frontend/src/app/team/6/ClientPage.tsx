2a04b28e64b93df16798f29ded7c4bbd819ccda5
chacha86 <taejincha0619@gmail.com>
2025-05-02 15:32:47 +0900

    work

--- frontend/src/app/team/6/ClientPage.tsx
+++ frontend/src/app/team/6/ClientPage.tsx
@@ -7,11 +7,11 @@ export default function ClinetPage() {
     <>
       <div>회원 로그인 페이지</div>
       <LoginForm2
-        url="http://api.main.ticketone.site:9000/api/v1/users/login"
+        url="https://api.app.mm.ts0608.life/api/auth/login"
         nameKey="email"
         passKey="password"
-        nameValue="test@test.com"
-        passValue="test"
+        nameValue="initUser1@example.com"
+        passValue="testPassword123!"
       />
     </>
   );
